5c0c90026b5cb87877d1a54d65ef861772c660d8
Ken Lu <ken@seeice.com>
2013-04-01 23:47:14 -0400

    added user sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessible :email, :password_digest
+  attr_accessible :email, :password_digest, :password, :password_confirmation
   validates_uniqueness_of :email
 end
