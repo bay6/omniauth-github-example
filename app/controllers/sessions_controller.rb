1a158c532e94276510c61e34527b7704f35a2ffe
Ken Lu <ken@seeice.com>
2013-04-02 08:36:06 -0400

    error message while fail authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       redirect_to root_url, notice: "Logged in!"
     else
       flash.now.alert = "Email or password is invalid."
+      redirect_to new_session_path, notice: 'Email or password is invalid'
     end
   end
 
