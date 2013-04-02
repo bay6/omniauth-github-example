0f467cbca893cf9aaef54997480ad84f0cecc271
Ken Lu <ken@seeice.com>
2013-04-02 08:55:47 -0400

    store info at controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create_github
-   raise env['omniauth.auth'].to_yaml 
+    user = User.from_omniauth(env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in."
   end
 
   def create
