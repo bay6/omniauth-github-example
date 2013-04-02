9db12e110543c0d444a49be637400380a0f35770
Ken Lu <ken@seeice.com>
2013-04-02 00:00:32 -0400

    sign up sign in and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: "Thank you for signing up!"
     else
       render "new"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Crm::Application.routes.draw do
-  get "sessions/new"
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :clients
-
-
+  resources :sessions
   resources :users
 
 
