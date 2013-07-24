2fd5179e385ed6c9c95e687b83bc0477c244ad85
William Herry <WilliamHerryChina@Gmail.com>
2013-07-25 01:17:20 +0800

    william's version of omniauth github example

---
+++ william/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+William::Application.config.secret_key_base = '04a1c429d588a3ea90934ee6ebfad369060e7912c7b9cdb84139139a1bafd0c71a66004fa15207d5040fc08a98f94e7b1041c44565b90304ed3a8d90598a8a48'
