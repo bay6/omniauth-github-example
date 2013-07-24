34d7c2442ca28ad6f1b89c45f16e5882e2d449ae
William Herry <WilliamHerryChina@Gmail.com>
2013-07-25 02:29:51 +0800

    user can sign out

--- william/config/routes.rb
+++ william/config/routes.rb
@@ -3,6 +3,7 @@ William::Application.routes.draw do
   resources :users
 
   get "/auth/:provider/callback" => "sessions#create"
+  get "signout" => "sessions#destroy", :as => "signout"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
